541b8149629f890ef46aef596eaa6e237066d9de
RuslanGon <ruslan441006@gmail.com>
2025-02-03 15:40:34 +0200

    wer

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${nunitoSans.variable} ${geistMono.variable} antialiased`}>
           <main className="bg-gray-100 min-h-screen flex flex-col items-center">
             <nav className="w-full bg-white h-16 flex items-center justify-center">
-              <section className="container flex items-center gap-3">
+              <section className="container flex items-center gap-3" >
                 <Image src='/logo.jpg' alt="logo" width={48} height={48}/>
                 <h1 className="font-bold text-2xl">Countries App</h1>
               </section>
