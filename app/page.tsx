d4479b251e71ff1336f58ae0a1cee26414d2d11c
RuslanGon <ruslan441006@gmail.com>
2025-02-03 16:44:30 +0200

    wer

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ type Country = {
 
 async function getCountries(): Promise<Country[]> {
   const response = await fetch("https://restcountries.com/v3.1/all");
-  const data = await response.json();  
-  console.log(data); 
-  return data; 
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 export default async function Home() {
@@ -18,11 +18,10 @@ export default async function Home() {
 
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-16">
-       {countries.map((country) => (
+      {countries.map((country) => (
         <>
-<article
-        className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl ">
-        {/* <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl ">
+            {/* <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
           <Image
             src=''
             alt=''
@@ -31,10 +30,9 @@ export default async function Home() {
             className="object-cover"
           />
         </div> */}
-        <h1 className="font-bold text-xl text-center mt-1">{country.name.common}</h1>
-      </article>
+            <h1 className="font-bold text-xl text-center mt-1">{country.name.common}</h1>
+          </article>
         </>
-        
       ))}
     </section>
   );
