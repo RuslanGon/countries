a6a788b906cb8433480bf8b835dc628a9e088c45
RuslanGon <ruslan441006@gmail.com>
2025-02-03 16:23:35 +0200

    wer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 
+type Country = {
+  name: {
+    common: string;
+  };
+};
 
-export default function Home() {
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
   return (
-    <section>
-
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-16">
+      {countries.map((country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
     </section>
   );
 }
