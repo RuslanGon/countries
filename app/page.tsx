7436f313ef475523a02077a927e4828acc9be547
RuslanGon <ruslan441006@gmail.com>
2025-02-04 10:19:36 +0200

    wer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 
 import Image from "next/image";
+import Link from "next/link";
 
 type Country = {
   name: {
@@ -26,6 +27,7 @@ export default async function Home() {
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-16">
       {countries.map((country) => (
         <>
+        <Link href={`/country/${country.name.common}`}>
           <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl ">
             <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
           <Image
@@ -37,6 +39,7 @@ export default async function Home() {
         </div>
             <h1 className="font-bold text-xl text-center mt-1">{country.name.common}</h1>
           </article>
+          </Link>
         </>
       ))}
     </section>
