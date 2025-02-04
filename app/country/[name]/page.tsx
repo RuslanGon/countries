2e65b285912f3f0d606494eab55efc75c3e1f729
RuslanGon <ruslan441006@gmail.com>
2025-02-04 10:38:02 +0200

    wer

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 
 async function getCountryByName(name: string) {
     const response  = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
@@ -6,9 +8,15 @@ async function getCountryByName(name: string) {
 }
 
 export default async function CountryDetail({params: {name}} : {params: {name: string}}) {
+    const country = await getCountryByName(name)
     return (
-        <section>
-            <h1>{name}</h1>
+        <section className="flex flex-col container">
+            <h1 className="text-5xl font-bold text-center text-gray-800 mt-16">
+                {country.translations.rus.official}
+            </h1>
+            <Link href="/" className="flex items-center py-2 gap-1">
+            <Image src="/arrow.svg" alt="go back home" width={24} height={24} />
+            </Link>
         </section>
     )
 }
