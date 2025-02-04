e98b54b07ac0a93225b20b769d454cc119120672
RuslanGon <ruslan441006@gmail.com>
2025-02-04 10:31:14 +0200

    wer

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -1,5 +1,9 @@
 
-
+async function getCountryByName(name: string) {
+    const response  = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+    const country = await response.json();
+    return country[0];
+}
 
 export default async function CountryDetail({params: {name}} : {params: {name: string}}) {
     return (
